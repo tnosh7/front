ec8a3d8bfa8bc9742d9495e48772031a5dd0ca33
Gyulo94 <qkrcjfrb112@gmail.com>
2024-08-30 02:07:54 +0900

    test

--- src/pages/Store/index.tsx
+++ src/pages/Store/index.tsx
@@ -2,7 +2,7 @@ import "./style.css";
 export default function Store() {
   return (
     <div id="store-wrapper">
-      <div className="store-container">스토어</div>
+      <div className="store-container">상점</div>
     </div>
   );
 }
