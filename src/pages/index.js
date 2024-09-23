32aacf5010528a72322ade79e4d17094042c8b1b
Gyulo94 <qkrcjfrb112@gmail.com>
2024-09-24 01:18:18 +0900

    페이지 index 작성

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
+import Auth from "./Auth";
 import Community from "./Community";
 import Game from "./Game";
 import Main from "./Main";
 import News from "./News";
 import Store from "./Store";
 
-export { Community, Game, Main, News, Store };
+export { Auth, Community, Game, Main, News, Store };
+
