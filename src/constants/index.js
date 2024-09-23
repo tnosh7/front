74af99e7f860846cf21b2ed3e81f96dbd04fca47
Gyulo94 <qkrcjfrb112@gmail.com>
2024-09-24 01:17:41 +0900

    AUTH_PATH, COM_PATH, GAME_PATH, MAIN_PATH, NEWS_PATH, STORE_PATH 라우팅 작성

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,5 +3,7 @@ const NEWS_PATH = () => "/news";
 const GAME_PATH = () => "/game";
 const COM_PATH = () => "/community";
 const STORE_PATH = () => "/store";
+const AUTH_PATH = () => "/auth";
 
-export { COM_PATH, GAME_PATH, MAIN_PATH, NEWS_PATH, STORE_PATH };
+export { AUTH_PATH, COM_PATH, GAME_PATH, MAIN_PATH, NEWS_PATH, STORE_PATH };
+
