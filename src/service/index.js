6bc875d2e80066efec77e4ca61753691cc41cf5b
Gyulo94 <qkrcjfrb112@gmail.com>
2024-09-24 01:21:16 +0900

    Login, Signup Axios 작성

--- src/service/index.js
+++ src/service/index.js
@@ -1,1 +1,46 @@
-export const getAppList = () => {};
+import axios from "axios";
+
+const DOMAIN = "http://localhost:8080";
+
+const API_DOMAIN = `${DOMAIN}/api`;
+
+const authorization = (accessToken) => {
+  return {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+};
+
+const LOGIN_URL = () => `${API_DOMAIN}/auth/login`;
+const SIGNUP_URL = () => `${API_DOMAIN}/auth/signup`;
+
+export const loginRequest = async (requestBody) => {
+  const result = await axios
+    .post(LOGIN_URL(), requestBody)
+    .then((response) => {
+      const responseBody = response.data;
+      return responseBody;
+    })
+    .catch((error) => {
+      if (!error.response) return null;
+      const responseBody = error.response.data;
+      return responseBody;
+    });
+  return result;
+};
+
+export const signupRequest = async (requestBody) => {
+  const result = await axios
+    .post(SIGNUP_URL(), requestBody)
+    .then((response) => {
+      const responseBody = response.data;
+      return responseBody;
+    })
+    .catch((error) => {
+      if (!error.response) return null;
+      const responseBody = error.response.data;
+      return responseBody;
+    });
+  return result;
+};
